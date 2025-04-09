862ab139695187d561496796b84b7065a3bc62cf
s-maki-p <sugar.maki.3316@gmail.com>
2025-04-09 16:39:01 +0900

    kadai_021をアップロード

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,19 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+
+		Dictionary_Chapter21 dic = new Dictionary_Chapter21();
+		
+		String[] search = {
+			"apple",
+			"banana",
+			"grape",
+			"orange"
+		};
+		
+		dic.displayDictionary(search);
+	}
+
+}
